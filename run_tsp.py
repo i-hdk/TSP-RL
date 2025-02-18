559070426dcdbc2e3ba5105ef62352f28a7f4bed
Isabella Lin <59080145+i-hdk@users.noreply.github.com>
2025-02-18 16:24:51 +0800

    add stable baselines

--- run_tsp.py
+++ run_tsp.py
@@ -1,3 +1,17 @@
 import gymnasium
 import tsp
+from stable_baselines.common.policies import MlpPolicy
+from stable_baselines.common.vec_env import DummyVecEnv
+from stable_baselines import PPO2
+
 env = gymnasium.make('tsp/TSP-v0')
+
+model = PPO2(MlpPolicy, env, verbose=1)
+model.learn(total_timesteps=10000)
+
+obs = env.reset()
+for i in range(1000):
+    action, _states = model.predict(obs)
+    obs, rewards, dones, info = env.step(action)
+    env.render()
+
